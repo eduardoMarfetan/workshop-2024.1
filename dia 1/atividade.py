8373d207564e23467fe1985e7c570575880ea253
eduardoMarfetan <edu.marfetan@gmail.com>
2024-03-05 14:13:50 -0300

    Create atividade.py

---
+++ dia 1/atividade.py
@@ -0,0 +1,19 @@
+lista = ['int', 'float', 'String', 'boolean'];
+
+dicionario = {'inteiro':'int', 'quebrado':'float', 'texto':'String', 'men':'boolean'};
+
+
+slacara = 10;
+naosei = 2;
+slasei = slacara * naosei;
+
+print (lista)
+print (slasei)
+print (dicionario['inteiro'])
+
+
+f = 2.35567557
+a = 2343.64654
+fa = a/f
+
+print(fa)
